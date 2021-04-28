002a7cc5eb9ab6caabb79f493f13d911a290a749
zengguanghao <zengguanghao@bonc.com.cn>
2021-04-28 16:22:03 +0800

    flink 的  trigger相关的测试

---
+++ src/main/java/xuwei/tech/window/trigger/SocketTriggerTest.java
@@ -0,0 +1,46 @@
+package xuwei.tech.window.trigger;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+/*
+trigger 测试
+滚动窗口，20s
+然后是trigger内部技术，10个元素输出一次。
+*/
+public class SocketTriggerTest {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> socketStream = env.socketTextStream("10.130.7.202", 10008, "\n");
+
+        AllWindowedStream<Integer, TimeWindow> stream = socketStream
+                .map(new String2Integer())
+                .timeWindowAll(org.apache.flink.streaming.api.windowing.time.Time.seconds(20))
+                .trigger(CountTrigger.create());
+        stream.sum(0)
+                .print();
+
+        env.execute("Flink Streaming Java API Skeleton");
+    }
+
+    private static class String2Integer extends RichMapFunction<String, Integer> {
+        private static final long serialVersionUID = 1180234853172462378L;
+
+        @Override
+        public Integer map(String event) throws Exception {
+            return Integer.valueOf(event);
+        }
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+        }
+    }
+
+}
